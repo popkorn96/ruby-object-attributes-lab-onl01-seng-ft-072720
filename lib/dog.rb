b5c5bb4e51ae85ec668b2fcdeeaed90ef764b7da
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-04 20:03:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 class Dog 
   def name=(dogs_name)
   @name = dogs_name
-end
-def name 
+  end
+  def name 
   @name 
-end
-  
+  end
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  def breed 
+    @breed
+  end
+    
 end
